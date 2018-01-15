3d3bdde31314250ab6e261464114376731fd6574
郭伟 <esphy@esphydembp.lan>
2018-01-15 15:11:04 +0800

    Initial Commit

---
+++ IMOOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IMOOC
+//
+//  Created by Esphy on 15/01/2018.
+//  Copyright © 2018 Esphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IMOOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IMOOC
+//
+//  Created by Esphy on 15/01/2018.
+//  Copyright © 2018 Esphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
